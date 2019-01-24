88e94c73b1ec6ad404293fa57bbadf9bfbffa7be
grahamwho <grahamwho@gmail.com>
2019-01-24 16:21:08 -0600

    Merge remote-tracking branch 'upstream/master'

--- src/main/target/OMNIBUS/target.mk
+++ src/main/target/OMNIBUS/target.mk
@@ -2,10 +2,10 @@ F3_TARGETS  += $(TARGET)
 
 FEATURES    = VCP SDCARD_SPI
 
-FEATURE_CUT_LEVEL = 9
+FEATURE_CUT_LEVEL = 6
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/barometer/barometer_bmp280.c \
-            drivers/max7456.c 
+            drivers/max7456.c
